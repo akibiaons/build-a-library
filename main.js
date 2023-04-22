bcdd11e792b3b9660a31e8a473126cec6a5a50c8
Arthur Fernandez <arthur@snooget.com>
2023-04-21 20:43:43 -0700

    Added the Media class, ocnstructor and title

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// Library code will be here for this project.
+// Library code will be here for this project. This library carries books, CD's, and movies.
+/*
+- I will create a parent class that includes shared proeprties such as titles and authors.
+*/
+
+// Below I will create the parent Media class
+class Media {
+  constructor(title) {
+    this._title = title;
+  }
+}
