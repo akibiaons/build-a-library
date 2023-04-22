40b26c19ad1bc2b5ec04276313af0405d558d45b
Arthur Fernandez <arthur@snooget.com>
2023-04-22 08:33:01 -0700

    added a movie class with media subproperties

--- main.js
+++ main.js
@@ -59,5 +59,21 @@ class Media {
     }
   }
 
+  class Movie extends Media {
+    constructor(director, title, runTime) {
+      super(title);
+      this.director = director;
+      this._runTime = runTime;
+    }
+
+    get director() {
+      return this._director; 
+    }
+
+    get runTime() {
+      return this._runTime;
+    }
+  }
+
 console.log(Media.isCheckedOut);
 console.log(Media._isCheckedOut);
